82ec689c28394a3ed7e3345d8f74a91f9be160e8
David Ballester Mena <david.ballester.mena@gmail.com>
2019-03-03 17:41:50 +0100

    feat: add graph list to the app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
 import { store } from '../../modules/store';
+import GraphList from '../../containers/GraphList';
 
 export class App extends Component {
   render() {
-    return <Provider store={store} />;
+    return (
+      <Provider store={store}>
+        <GraphList />
+      </Provider>
+    );
   }
 }
