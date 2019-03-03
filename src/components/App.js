868c39b7c799b3e722a6556ca5af45d345d032e7
David Ballester Mena <david.ballester.mena@gmail.com>
2019-03-03 20:59:00 +0100

    feat: add new graph to the app

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import { Provider } from 'react-redux';
 
 import { store } from '../modules/store';
 import GraphList from '../containers/GraphList';
+import NewGraph from '../containers/NewGraph';
 
 export class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <GraphList />
+        <NewGraph />
       </Provider>
     );
   }
