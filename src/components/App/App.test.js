33baac0c3e2661caa8daf15b5f8be4a871ccda87
David Ballester Mena <david.ballester.mena@gmail.com>
2019-03-03 17:39:14 +0100

    test: use enzyme in App test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { App } from './App';
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const component = shallow(<App />);
+    expect(component).toBeDefined();
+  });
 });
