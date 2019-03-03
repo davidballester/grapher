77e7294af0eb4ff3ed5e0a65af25a42763791bbf
David Ballester Mena <david.ballester.mena@gmail.com>
2019-03-03 17:43:51 +0100

    refactor: remove App folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
-import { store } from '../../modules/store';
-import GraphList from '../../containers/GraphList';
+import { store } from '../modules/store';
+import GraphList from '../containers/GraphList';
 
 export class App extends Component {
   render() {
