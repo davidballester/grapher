8721af24867aef519bcd1f9dcd45653e5e88b476
David Ballester Mena <david.ballester.mena@gmail.com>
2019-03-03 20:57:33 +0100

    feat: configure graph and new-graph reducers in store

--- src/modules/store.js
+++ src/modules/store.js
@@ -3,11 +3,15 @@ import createSagaMiddleware from 'redux-saga';
 
 import graphNamesReducer from './graph-names';
 import graphListReducer from './graph-list';
+import graphReducer from './graph';
+import newGraphReducer from './new-graph';
 
 const sagaMiddleware = createSagaMiddleware();
 const reducer = combineReducers({
   graphNames: graphNamesReducer,
   graphList: graphListReducer,
+  graph: graphReducer,
+  newGraph: newGraphReducer,
 });
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(reducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
