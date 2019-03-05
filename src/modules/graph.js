d30f78b01e52d652f285d3973fcf7da9f732faa8
David Ballester Mena <david.ballester.mena@gmail.com>
2019-03-05 20:02:25 +0100

    feat: include graph data in graph module

--- src/modules/graph.js
+++ src/modules/graph.js
@@ -1,7 +1,11 @@
+import { createSelector } from 'reselect';
+
 export const GRAPH_SET_NAME = 'grapher/Graph/SET_NAME';
 
 const initialState = {
   name: '',
+  nodes: {},
+  links: {},
 };
 
 export default function reducer(state = initialState, action) {
@@ -32,3 +36,21 @@ function graphSelector(state) {
 export function getName(state) {
   return graphSelector(state).name;
 }
+
+function getNodes(state) {
+  return graphSelector(state).nodes;
+}
+
+function getLinks(state) {
+  return graphSelector(state).links;
+}
+
+export const getNodesAsArray = createSelector(
+  getNodes,
+  (nodes) => Object.values(nodes)
+);
+
+export const getLinksAsArray = createSelector(
+  getLinks,
+  (links) => Object.values(links)
+);
--- src/modules/graph.spec.js
+++ src/modules/graph.spec.js
@@ -1,4 +1,4 @@
-import { getName, setNameGraph, GRAPH_SET_NAME } from './graph';
+import { getName, setNameGraph, GRAPH_SET_NAME, getNodesAsArray, getLinksAsArray } from './graph';
 import reducer from './graph';
 
 describe('graph', () => {
@@ -43,6 +43,64 @@ describe('graph', () => {
         };
         const name = getName(appState);
         expect(name).toEqual(expectedName);
+      });
+    });
+
+    describe('getNodesAsArray', () => {
+      it('returns an empty array if there are no nodes', () => {
+        const appState = {
+          graph: {
+            nodes: {},
+          },
+        };
+        const nodes = getNodesAsArray(appState);
+        expect(nodes).toEqual([]);
+      });
+
+      it('returns an array with all the nodes in the state', () => {
+        const node1 = { bar: 'baz' };
+        const node2 = { baz: 'qux' };
+        const node3 = { qux: 'quux' };
+        const appState = {
+          graph: {
+            nodes: {
+              foo: node1,
+              bar: node2,
+              baz: node3,
+            },
+          },
+        };
+        const nodes = getNodesAsArray(appState);
+        expect(nodes).toEqual([node1, node2, node3]);
+      });
+    });
+
+    describe('getLinksAsArray', () => {
+      it('returns an empty array if there are no links', () => {
+        const appState = {
+          graph: {
+            links: {},
+          },
+        };
+        const links = getLinksAsArray(appState);
+        expect(links).toEqual([]);
+      });
+
+      it('returns an array with all the links in the state', () => {
+        const link1 = { bar: 'baz' };
+        const link2 = { baz: 'qux' };
+        const link3 = { qux: 'quux' };
+        const appState = {
+          graph: {
+            links: {
+              foo: link1,
+              bar: link2,
+              baz: link3,
+            },
+          },
+        };
+        const links = getLinksAsArray(appState);
+        expect(links).toEqual([link1, link2, link3]);
       });
     });
   });
