3643f4c27cef7b51a47606523171158c9a5c3df6
David Ballester Mena <david.ballester.mena@gmail.com>
2019-03-06 19:15:22 +0100

    feat: add labels to nodes

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -11,8 +11,25 @@ function renderCircle(node, ctx) {
   ctx.fill();
 }
 
-function renderNode(node, ctx) {
+function renderLabel(node, ctx, globalScale) {
+  const label = node.id;
+  const fontSize = 16 / globalScale;
+
+  const textWidth = ctx.measureText(label).width;
+  const bckgDimensions = [textWidth + fontSize * 0.2, fontSize * 1.2];
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.6)';
+  ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y + 14 - bckgDimensions[1] / 2, ...bckgDimensions);
+
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.fillStyle = '#000';
+  ctx.font = `${fontSize}px Sans-Serif`;
+  ctx.fillText(node.id, node.x, node.y + 12);
+}
+
+function renderNode(node, ctx, globalScale) {
   renderCircle(node, ctx);
+  renderLabel(node, ctx, globalScale);
 }
 
 export default class Canvas extends React.PureComponent {
