0b6c4fa1e102fe1b25915fe5435286ce532c348b
David Ballester Mena <david.ballester.mena@gmail.com>
2019-03-24 18:55:59 +0100

    feat: add save and remove graph names methods to service

--- src/services/graph-names-service.js
+++ src/services/graph-names-service.js
@@ -5,6 +5,25 @@ class GraphNamesService {
     const rawGraphNames = localStorage.getItem(GRAPHS_NAMES_STORAGE_KEY);
     return !!rawGraphNames ? JSON.parse(rawGraphNames) : [];
   }
+
+  saveGraphName(graphName) {
+    const graphNames = this.getGraphNames();
+    const newGraphNames = [...graphNames, graphName];
+    this.saveGraphNames(newGraphNames);
+  }
+
+  removeGraphName(graphName) {
+    const graphNames = this.getGraphNames();
+    const graphNameIndex = graphNames.findIndex((gn) => gn === graphName);
+    if (graphNameIndex >= 0) {
+      const newGraphNames = graphNames.filter((_, index) => index !== graphNameIndex);
+      this.saveGraphNames(newGraphNames);
+    }
+  }
+
+  saveGraphNames(graphNames) {
+    localStorage.setItem(GRAPHS_NAMES_STORAGE_KEY, JSON.stringify(graphNames));
+  }
 }
 
 export const graphNamesService = new GraphNamesService();
--- src/services/graph-names-service.spec.js
+++ src/services/graph-names-service.spec.js
@@ -1,15 +1,15 @@
 import { graphNamesService, GRAPHS_NAMES_STORAGE_KEY } from './graph-names-service';
 
 describe('GraphNamesService', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   describe('#getGraphNames', () => {
     let getItemSpy;
 
     beforeEach(() => {
       getItemSpy = jest.spyOn(localStorage, 'getItem').mockReturnValue('[]');
-    });
-
-    afterEach(() => {
-      jest.resetAllMocks();
     });
 
     it('reads the GRAPHS_NAMES_STORAGE_KEY key from the local storage', () => {
@@ -28,6 +28,58 @@ describe('GraphNamesService', () => {
       getItemSpy.mockReturnValue(JSON.stringify(expectedGraphNames));
       const graphNames = graphNamesService.getGraphNames();
       expect(graphNames).toEqual(expectedGraphNames);
+    });
+  });
+
+  describe('#saveGraphName', () => {
+    let graphNames;
+    let graphName;
+    let setItemSpy;
+
+    beforeEach(() => {
+      graphNames = ['foo'];
+      graphName = 'bar';
+      jest.spyOn(localStorage, 'getItem').mockReturnValue(JSON.stringify(graphNames));
+      setItemSpy = jest.spyOn(localStorage, 'setItem');
+    });
+
+    it('invokes `localStorage.setItem` with the existing array of graph names + the provided graph name', () => {
+      graphNamesService.saveGraphName(graphName);
+      expect(setItemSpy).toHaveBeenCalledWith(GRAPHS_NAMES_STORAGE_KEY, JSON.stringify([...graphNames, graphName]));
+    });
+
+    it('adds a graph name even if it was already in the list', () => {
+      graphNamesService.saveGraphName(graphNames[0]);
+      expect(setItemSpy).toHaveBeenCalledWith(GRAPHS_NAMES_STORAGE_KEY, JSON.stringify([...graphNames, graphNames[0]]));
+    });
+  });
+
+  describe('#removeGraphName', () => {
+    let graphNames;
+    let graphName;
+    let setItemSpy;
+
+    beforeEach(() => {
+      graphNames = ['foo'];
+      graphName = 'foo';
+      jest.spyOn(localStorage, 'getItem').mockReturnValue(JSON.stringify(graphNames));
+      setItemSpy = jest.spyOn(localStorage, 'setItem');
+    });
+
+    it('invokes `localStorage.setItem` with the existing array of graph names - the provided graph name', () => {
+      graphNamesService.removeGraphName(graphName);
+      expect(setItemSpy).toHaveBeenCalledWith(GRAPHS_NAMES_STORAGE_KEY, JSON.stringify([]));
+    });
+
+    it('removes just one instance of the provided name', () => {
+      jest.spyOn(localStorage, 'getItem').mockReturnValue(JSON.stringify([...graphNames, ...graphNames]));
+      graphNamesService.removeGraphName(graphName);
+      expect(setItemSpy).toHaveBeenCalledWith(GRAPHS_NAMES_STORAGE_KEY, JSON.stringify(graphNames));
+    });
+
+    it('does not save the array if the provided name is not contained', () => {
+      graphNamesService.removeGraphName('bar');
+      expect(setItemSpy).not.toHaveBeenCalled();
     });
   });
 });
