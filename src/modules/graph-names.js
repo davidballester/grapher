a12f5add6baa8a3c70c470f521b696b01a37c8d3
David Ballester Mena <david.ballester.mena@gmail.com>
2019-03-31 14:21:54 +0200

    feat: initialize graph names store with stored graph names

--- src/modules/graph-names.js
+++ src/modules/graph-names.js
@@ -1,4 +1,6 @@
-const initialState = [];
+import graphNamesService from '../services/graph-names-service';
+
+const initialState = graphNamesService.getGraphNames();
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
