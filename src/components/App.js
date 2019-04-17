20f73163f3e86da8d5d7c6de9a8e5ac6351933ff
David Ballester Mena <david.ballester.mena@gmail.com>
2019-04-17 22:17:10 +0200

    refactor: move redux and router boilerplate to index

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
-import { store } from '../modules/store';
-import history from '../services/history';
 import GraphList from '../containers/GraphList';
 import NewGraph from '../containers/NewGraph';
 import Canvas from '../containers/Canvas';
@@ -12,14 +9,12 @@ import EditGraph from './EditGraph';
 export class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Router history={history}>
-          <Canvas />
-          <Route path="/" exact component={GraphList} />
-          <Route path="/graph/new" exact component={NewGraph} />
-          <Route path="/graph/:graphName" exact component={EditGraph} />
-        </Router>
-      </Provider>
+      <React.Fragment>
+        <Canvas />
+        <Route path="/" exact component={GraphList} />
+        <Route path="/graph/new" exact component={NewGraph} />
+        <Route path="/graph/:graphName" exact component={EditGraph} />
+      </React.Fragment>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Router } from 'react-router-dom';
+
+import { store } from './modules/store';
+import history from './services/history';
 import { App } from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
