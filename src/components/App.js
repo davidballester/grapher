ce930286f497da0a99695a1a3c4f036e8eb19845
David Ballester Mena <david.ballester.mena@gmail.com>
2019-04-17 22:10:57 +0200

    feat: add routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router-dom';
 
 import { store } from '../modules/store';
+import history from '../services/history';
 import GraphList from '../containers/GraphList';
 import NewGraph from '../containers/NewGraph';
 import Canvas from '../containers/Canvas';
-import NewNode from '../containers/NewNode';
-import ConfirmDeleteNode from '../containers/ConfirmDeleteNode';
-import ConfirmDeleteLink from '../containers/ConfirmDeleteLink';
-import SelectedItems from './SelectedItems';
-import EditNode from '../containers/EditNode';
+import EditGraph from './EditGraph';
 
 export class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <GraphList />
-        <NewGraph />
-        <Canvas />
-        <NewNode />
-        <ConfirmDeleteNode />
-        <ConfirmDeleteLink />
-        <SelectedItems />
-        <EditNode />
+        <Router history={history}>
+          <Canvas />
+          <Route path="/" exact component={GraphList} />
+          <Route path="/graph/new" exact component={NewGraph} />
+          <Route path="/graph/:graphName" exact component={EditGraph} />
+        </Router>
       </Provider>
     );
   }
