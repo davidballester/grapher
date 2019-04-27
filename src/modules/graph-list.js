9072ef48e1a82fb1ba1801fd2b646638b070ea40
David Ballester Mena <david.ballester.mena@gmail.com>
2019-04-27 22:48:38 +0200

    fix: update graph list based on actions

--- src/modules/graph-list.js
+++ src/modules/graph-list.js
@@ -2,13 +2,38 @@ import { createSelector } from 'reselect';
 import * as _entries from 'lodash/entries';
 
 import graphNamesService from '../services/graph-names-service';
+import { GRAPH_SET_NAME, GRAPH_CREATE, GRAPH_DELETE } from './graph';
 
 export const GRAPH_LIST_OPEN = 'grapher/GraphList/OPEN';
 
 const initialState = graphNamesService.getGraphNames();
 
 export default function reducer(state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case GRAPH_CREATE: {
+      const { id, name } = action.payload;
+      return {
+        ...state,
+        [id]: name,
+      };
+    }
+    case GRAPH_DELETE: {
+      const { payload: graphId } = action;
+      const newState = { ...state };
+      delete newState[graphId];
+      return newState;
+    }
+    case GRAPH_SET_NAME: {
+      const { id, name } = action.payload;
+      return {
+        ...state,
+        [id]: name,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 }
 
 export function openGraphList() {
