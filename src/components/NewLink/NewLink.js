5caaa1eeecf032a04d8aaf5b41d6b51b4ed1af53
David Ballester Mena <david.ballester.mena@gmail.com>
2019-06-24 19:13:47 +0200

    fix: combobox overflow regression

--- src/components/NewLink/NewLink.js
+++ src/components/NewLink/NewLink.js
@@ -6,12 +6,12 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogContent';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/styles';
 import { TextField } from 'formik-material-ui';
 import { Formik, Field, Form } from 'formik';
 import * as Yup from 'yup';
 
 import NodeSearcher from './NodeSearcher';
+import './NewLink.css';
 
 const StyledTextField = styled(TextField)`
   display: block !important;
@@ -21,21 +21,7 @@ const StyledNodeSearcher = styled(NodeSearcher)`
   margin-top: 1rem;
 `;
 
-const StyledDialogContent = styled(DialogContent)`
-  overflow-y: visible !important;
-`;
-
-const useStyles = makeStyles({
-  root: {
-    '& *': {
-      overflowY: 'visible',
-    },
-  },
-});
-
 function NewLink({ isOpen, nodesIds, saveNewLink, cancelNewLink }) {
-  const classes = useStyles();
-
   const initialValues = {
     label: '',
     source: undefined,
@@ -49,7 +35,7 @@ function NewLink({ isOpen, nodesIds, saveNewLink, cancelNewLink }) {
   });
 
   return (
-    <Dialog open={isOpen} className={classes.root}>
+    <Dialog open={isOpen}>
       <DialogTitle>New link</DialogTitle>
       <Formik
         initialValues={initialValues}
@@ -62,11 +48,11 @@ function NewLink({ isOpen, nodesIds, saveNewLink, cancelNewLink }) {
         }
         render={({ errors, setFieldValue }) => (
           <Form>
-            <StyledDialogContent>
+            <DialogContent>
               <Field type="text" label="Label" name="label" component={StyledTextField} error={!!errors.label} />
               <StyledNodeSearcher nodesIds={nodesIds} onChange={(source) => setFieldValue('source', source)} label="Source" />
               <StyledNodeSearcher nodesIds={nodesIds} onChange={(target) => setFieldValue('target', target)} label="Target" />
-            </StyledDialogContent>
+            </DialogContent>
             <DialogActions>
               <Button onClick={cancelNewLink} className="cancel" type="button">
                 Cancel
