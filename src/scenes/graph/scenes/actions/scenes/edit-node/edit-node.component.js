4bfb96f556ccfbec149e9a8d03c6cf3738d23761
David Ballester Mena <david.ballester.mena@gmail.com>
2019-07-07 20:01:45 +0200

    feat: add groups to node edition

--- src/scenes/graph/scenes/actions/scenes/edit-node/edit-node.component.js
+++ src/scenes/graph/scenes/actions/scenes/edit-node/edit-node.component.js
@@ -12,6 +12,7 @@ import * as Yup from 'yup';
 import { red, purple, blue, green, yellow, orange, brown, grey } from '@material-ui/core/colors';
 
 import ToggleColorPicker from '../../../../../../components/toggle-color-picker.component';
+import GroupsSelect from './components/groups-select.component';
 
 const StyledTextField = styled(TextField)`
   display: block !important;
@@ -19,17 +20,20 @@ const StyledTextField = styled(TextField)`
 
 const colors = [red['A200'], purple['A200'], blue['A200'], green['A200'], yellow['A200'], orange['A200'], brown['500'], grey['500']];
 
-function EditNode({ isOpen, node = {}, nodesIds = [], editNode, cancelEditNode }) {
+function EditNode({ isOpen, node = {}, nodesIds = [], groups = [], editNode, cancelEditNode }) {
+  const nodeGroups = node.groups || [];
   const oldId = node.id;
   const initialValues = {
     id: node.id,
     color: node.color,
+    groups: groups.filter((group) => !!nodeGroups.find((nodeGroup) => nodeGroup.id === group.id)),
   };
 
   const NodeSchema = Yup.object().shape({
     id: Yup.string()
       .required('Required')
       .notOneOf(nodesIds.filter((nodeId) => nodeId !== node.id)),
+    groups: Yup.array(),
   });
 
   return (
@@ -44,6 +48,7 @@ function EditNode({ isOpen, node = {}, nodesIds = [], editNode, cancelEditNode }
             <DialogContent>
               <Field type="text" label="ID" name="id" component={StyledTextField} error={!!errors.id} />
               <ToggleColorPicker color={values.color} colors={colors} onChange={(color) => setFieldValue('color', color)} />
+              <GroupsSelect groups={groups} selectedGroups={values.groups} onChange={(selectedGroups) => setFieldValue('groups', selectedGroups)} />
             </DialogContent>
             <DialogActions>
               <Button onClick={cancelEditNode} className="cancel" type="button">
