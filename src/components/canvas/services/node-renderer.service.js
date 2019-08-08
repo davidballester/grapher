47e41a2fb63620f5de6170573a1378aa86a6ca04
David Ballester Mena <david.ballester.mena@gmail.com>
2019-08-08 19:10:29 +0200

    feat(canvas): use sectioning coloring for node groups

--- src/components/canvas/services/node-renderer.service.js
+++ src/components/canvas/services/node-renderer.service.js
@@ -1,23 +1,16 @@
 import blue from '@material-ui/core/colors/blue';
 import orange from '@material-ui/core/colors/orange';
-import _get from 'lodash/get';
 
 export function getNodeColor(node) {
   const nodeColor = node.color;
-  if (!!nodeColor) {
-    return nodeColor;
-  }
-  const groupColor = _get(node, 'groups[0].color');
-  if (!!groupColor) {
-    return groupColor;
-  }
-  return blue['A200'];
+  return nodeColor || blue['A200'];
 }
 
 export function renderNode(node, ctx, globalScale) {
   if (node.selected) {
     renderSelectedCircle(node, ctx);
   }
+  renderGroups(node, ctx);
   renderLabel(node, ctx, globalScale);
 }
 
@@ -37,4 +30,21 @@ function renderLabel(node, ctx, globalScale) {
   ctx.textBaseline = 'middle';
   ctx.fillStyle = '#000';
   ctx.fillText(node.id, node.x, node.y + 8);
+}
+
+function renderGroups(node, ctx) {
+  const { groups = [] } = node;
+  if (!groups.length) {
+    return;
+  }
+  const sliceAngle = (Math.PI * 2) / groups.length;
+  ctx.save();
+  groups.forEach((group, index) => {
+    ctx.beginPath();
+    ctx.strokeStyle = group.color;
+    ctx.lineWidth = 4;
+    ctx.arc(node.x, node.y, 3, index * sliceAngle, (index + 1) * sliceAngle);
+    ctx.stroke();
+  });
+  ctx.restore();
 }
