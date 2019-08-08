46802fbc4b47e2d33f8e6e33da1609433f94d357
David Ballester Mena <david.ballester.mena@gmail.com>
2019-08-08 16:32:14 +0200

    fix(groups): use ellipsis for text when it does not fit

--- src/scenes/graph/components/groups/components/group-list-item.component.js
+++ src/scenes/graph/components/groups/components/group-list-item.component.js
@@ -5,25 +5,30 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
+import { withStyles } from '@material-ui/core/styles';
 
 import ColorBox from './color-box.component';
 
-export default function GroupListItemComponent({ group, onDelete, onEdit }) {
-  return (
-    <ListItem>
-      <ListItemIcon>
-        <ColorBox color={group.color} />
-      </ListItemIcon>
-      <ListItemText primary={group.name} />
-      <ListItemSecondaryAction edge="end">
-        <IconButton onClick={() => onEdit(group)}>
-          <EditIcon />
-        </IconButton>
-        <IconButton onClick={() => onDelete(group.id)}>
-          <DeleteIcon />
-        </IconButton>
-      </ListItemSecondaryAction>
-    </ListItem>
-  );
-}
+const styles = {
+  text: {
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+  },
+};
+
+const GroupListItemComponent = ({ group, onDelete, onEdit, classes }) => (
+  <ListItem button onClick={() => onEdit(group)}>
+    <ListItemIcon>
+      <ColorBox color={group.color} />
+    </ListItemIcon>
+    <ListItemText primary={group.name} classes={{ root: classes.text }} />
+    <ListItemSecondaryAction edge="end">
+      <IconButton onClick={() => onDelete(group.id)}>
+        <DeleteIcon />
+      </IconButton>
+    </ListItemSecondaryAction>
+  </ListItem>
+);
+
+export default withStyles(styles)(GroupListItemComponent);
