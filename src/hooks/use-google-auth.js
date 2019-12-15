a5d5777d58939cb100fa1f7d74417b3272ee9675
David Ballester Mena <david.ballester.mena@gmail.com>
2019-12-15 21:14:06 +0100

    fix: wait for google auth initialization before using it

--- src/hooks/use-google-auth.js
+++ src/hooks/use-google-auth.js
@@ -3,16 +3,7 @@ import { useState, useEffect } from 'react';
 export default function useGoogleAuth(onSignedIn) {
   const [googleAuth, setGoogleAuth] = useState(undefined);
   useEffect(() => {
-    window.gapi.load('auth2', async () => {
-      const newGoogleAuth = await window.gapi.auth2.init({
-        client_id: '763957914953-pl2b8vab3sjge1l1gnc05hra24bnrueg.apps.googleusercontent.com',
-      });
-      if (newGoogleAuth.isSignedIn.get() && !!onSignedIn) {
-        const basicProfile = newGoogleAuth.currentUser.get().getBasicProfile();
-        onSignedIn(getDataFromProfile(basicProfile));
-      }
-      setGoogleAuth(newGoogleAuth);
-    });
+    initializeGoogleAuth(onSignedIn, setGoogleAuth);
   }, [onSignedIn]);
 
   const signIn = async () => {
@@ -37,4 +28,21 @@ function getDataFromProfile(basicProfile) {
     name: basicProfile.getName(),
     imageUrl: basicProfile.getImageUrl(),
   };
+}
+
+function initializeGoogleAuth(onSignedIn, setGoogleAuth) {
+  if (!window.gapi) {
+    setTimeout(() => initializeGoogleAuth(onSignedIn, setGoogleAuth), 100);
+    return;
+  }
+  window.gapi.load('auth2', async () => {
+    const newGoogleAuth = await window.gapi.auth2.init({
+      client_id: '763957914953-pl2b8vab3sjge1l1gnc05hra24bnrueg.apps.googleusercontent.com',
+    });
+    if (newGoogleAuth.isSignedIn.get() && !!onSignedIn) {
+      const basicProfile = newGoogleAuth.currentUser.get().getBasicProfile();
+      onSignedIn(getDataFromProfile(basicProfile));
+    }
+    setGoogleAuth(newGoogleAuth);
+  });
 }
