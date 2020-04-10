e44789136a2fae247ac9ef02c9313457ff1f74dd
David Ballester Mena <david.ballester.mena@gmail.com>
2020-04-10 22:10:40 +0200

    chore: add firebase

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import firebase from 'firebase/app';
 
 import { store } from './store';
 import history from './services/history.service';
 import { App } from './app.component';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyB7acR5DaXQ-4shrslNA7Q2_1wjnDPMZM4',
+  authDomain: 'grapher-4e826.firebaseapp.com',
+  databaseURL: 'https://grapher-4e826.firebaseio.com',
+  projectId: 'grapher-4e826',
+  storageBucket: 'grapher-4e826.appspot.com',
+  messagingSenderId: '645009956208',
+  appId: '1:645009956208:web:e063f283adf8b220c0c3b5',
+  measurementId: 'G-YNY3VRTPYS',
+});
 
 ReactDOM.render(
   <Provider store={store}>
