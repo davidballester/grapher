c89cd4293d50dd63592dd2e41694252155f4f1b4
David Ballester Mena <david.ballester.mena@gmail.com>
2020-04-12 14:23:17 +0200

    feat: display an error if login fails

--- src/components/authentication/authentication.component.js
+++ src/components/authentication/authentication.component.js
@@ -3,11 +3,14 @@ import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Avatar from '@material-ui/core/Avatar';
+import Snackbar from '@material-ui/core/Snackbar';
 
 import { initializeAuthProvider, signIn } from '../../services/google-auth';
 import LoggedInPopover from '../logged-in-popover';
+import Alert from '../Alert';
 
 export default function Authentication({ setAuth, setAuthProvider, isSignedIn, imageUrl, authProvider }) {
+  const [error, setError] = useState(false);
   useEffect(() => {
     if (!authProvider) {
       initializeAuthProvider(({ id, name, imageUrl }) => setAuth(id, name, imageUrl), setAuthProvider);
@@ -15,15 +18,23 @@ export default function Authentication({ setAuth, setAuthProvider, isSignedIn, i
   }, [authProvider, setAuth, setAuthProvider]);
   if (!isSignedIn) {
     return (
-      <SignInButton
-        onSignIn={async () => {
-          const profile = await signIn(authProvider);
-          if (!!profile) {
-            const { id, name, imageUrl } = profile;
-            setAuth(id, name, imageUrl);
-          }
-        }}
-      />
+      <>
+        <SignInButton
+          onSignIn={async () => {
+            const profile = await signIn(authProvider);
+            if (!!profile) {
+              const { id, name, imageUrl } = profile;
+              setAuth(id, name, imageUrl);
+              setError(false);
+            } else {
+              setError(true);
+            }
+          }}
+        />
+        <Snackbar anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }} open={error} autoHideDuration={6000} onClose={() => setError(false)}>
+          <Alert severity="error">There was an error with your authentication</Alert>
+        </Snackbar>
+      </>
     );
   }
   return <SignedInButton imageUrl={imageUrl} />;
