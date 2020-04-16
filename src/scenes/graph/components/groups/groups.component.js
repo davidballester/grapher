3d4309a6681d90675517d589ed6b4164b7289faf
David Ballester Mena <david.ballester.mena@gmail.com>
2020-04-16 20:41:13 +0200

    feat: style scrollbar for groups card

--- src/scenes/graph/components/groups/groups.component.js
+++ src/scenes/graph/components/groups/groups.component.js
@@ -10,20 +10,36 @@ import List from '@material-ui/core/List';
 import EmptyState from './components/empty-state.component';
 import GroupListItem from './components/group-list-item.component';
 
-const styles = {
-  root: {
-    width: '100%',
-    '&.Mui-expanded': {
-      margin: 0,
+const styles = (theme) => {
+  console.log(theme.palette);
+  return {
+    root: {
+      width: '100%',
+      '&.Mui-expanded': {
+        margin: 0,
+      },
     },
-  },
-  list: {
-    width: '100%',
-  },
-  content: {
-    maxHeight: '30vh',
-    overflow: 'auto',
-  },
+    list: {
+      width: '100%',
+    },
+    content: {
+      maxHeight: '30vh',
+      overflow: 'auto',
+      'scrollbar-width': 'thin',
+      'scrollbar-color': `${theme.palette.text.primary} ${theme.palette.background.paper}`,
+      '&::-webkit-scrollbar': {
+        width: 11,
+      },
+      '&::-webkit-scrollbar-track': {
+        background: theme.palette.background.paper,
+      },
+      '&::-webkit-scrollbar-thumb': {
+        backgroundColor: theme.palette.text.primary,
+        borderRadius: 6,
+        border: `3px solid ${theme.palette.background.paper}`,
+      },
+    },
+  };
 };
 
 function Groups({ groups = [], classes = {} }) {
@@ -48,4 +64,4 @@ function Groups({ groups = [], classes = {} }) {
   );
 }
 
-export default withStyles(styles)(Groups);
+export default withStyles(styles, { withTheme: true })(Groups);
